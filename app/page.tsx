00b221c28d8828f7d58b46f7247c061ab342508c
HoaCode <hoacode@HoaCodes-MacBook-Pro.local>
2025-11-11 15:49:44 +0700

    next.js + prisma + postgresql

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
-
-export default function Home() {
+import prisma from '@/lib/prisma'
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <div>
-           <h1 className="w-full text-2xl">Welcome to the Home Page</h1>
-            <h2 className="w-full text-3xl text-red-500">Hoa Nguyen Coder</h2>
-      </div>
+     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Hoa Nguyen Coder
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2 text-red-400">
+            {user.name}
+          </li>
+        ))}
+      </ol>
+      
     </div>
   );
 }
