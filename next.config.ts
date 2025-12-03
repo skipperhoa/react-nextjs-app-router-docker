2bc2b549cc2d9819a73e2f0e386ab41801eda9e8
skipperhoa <thanhhoacth2013@gmail.com>
2025-12-03 09:35:52 +0700

    push code

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone", // standalone or export
+  output: "export", // standalone or export
 };
 
 export default nextConfig;
