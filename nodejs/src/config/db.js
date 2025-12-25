36e654c675dc4e0a07fed0bce2981457125093ba
skipperhoa <thanhhoacth2013@gmail.com>
2025-12-25 09:09:04 +0700

    fix host docker

--- nodejs/src/config/db.js
+++ nodejs/src/config/db.js
@@ -4,7 +4,7 @@ const { Pool } = pg
 const pool = new Pool({
   user: 'hoadev',
   password: 'hoadev123',
-  host: 'localhost',
+  host: 'host.docker.internal',
   port: 5432,
   database: 'hoadev_db',
 })
