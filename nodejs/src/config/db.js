874410267a61e85d20188a3379328c737201a381
skipperhoa <thanhhoacth2013@gmail.com>
2025-12-31 23:25:37 +0700

    modify connect postgres

--- nodejs/src/config/db.js
+++ nodejs/src/config/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT || 5432,
+  port: process.env.POSTGRES_PORT || 5432
 })
 
 export default pool
